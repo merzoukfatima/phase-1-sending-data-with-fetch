48557b50bf5173619336dfd180dd99ea535979ae
MerzoukFatimaZohra <merzoukfatimazahraa@gmail.com>
2023-08-28 01:41:02 +0100

    Assignment Completed

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Add your code here
+const body =document.querySelector("body")
+const submitData=((name,email)=>{
+    return fetch("http://localhost:3000/users",{
+        method:"POST",
+        headers:{
+            "Content-Type":"application/json",
+            "Accept":"application/json",
+        },
+        body:JSON.stringify({name:`${name}`,email:`${email}`})
+    }).then(res => res.json())
+      .then(data => body.textContent=data.id
+      )
+      .catch(error => body.textContent=error.message)
+})
